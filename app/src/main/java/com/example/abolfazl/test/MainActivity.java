ec2d1c3a0c12888ca9bc3bb82f7474dbfeaae79c
abolfazl <bateternal@gmail.com>
2017-07-31 20:52:08 +0430

    alam :wq
    :wq

--- app/src/main/java/com/example/abolfazl/test/MainActivity.java
+++ app/src/main/java/com/example/abolfazl/test/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,102 +17,106 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.abolfazl.test.tools.drawtheball;
+import com.example.abolfazl.test.tools.DrawTest;
 
-public class MainActivity extends Activity implements GestureDetector.OnGestureListener,
-GestureDetector.OnDoubleTapListener{
 
-    private TextView buckysMessage;
-    private GestureDetectorCompat gestureDetector;
-    drawtheball v;
+public class MainActivity extends Activity {
+
+
+
+    DrawTest v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        v = new drawtheball(this);
-        setContentView(R.layout.activity_main);
+
+        v = new DrawTest(this);
         setContentView(v);
-        buckysMessage = (TextView)findViewById(R.id.salami);
-        this.gestureDetector = new GestureDetectorCompat(this,this);
-        gestureDetector.setOnDoubleTapListener(this);
+
+
+
 
     }
 
 
 
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
-        //buckysMessage.setText("onSingleTapConfirmed");
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent motionEvent) {
-        //buckysMessage.setText("onDoubleTap");
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
-        //buckysMessage.setText("onDoubleTapEvent");
-        return true;
-    }
-
-    @Override
-    public boolean onDown(MotionEvent motionEvent) {
-        //buckysMessage.setText("onDown");
-        return true;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent motionEvent) {
-        //buckysMessage.setText("onShowPress");
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent motionEvent) {
-        //buckysMessage.setText("onSingleTapUp");
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        //buckysMessage.setText("onScroll");
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent motionEvent) {
-        //buckysMessage.setText("onLongPress");
-    }
-
-    @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        //buckysMessage.setText("onFling");
-        return false;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return false;
-    }
-
-    ////////////////// End Gestures ///////////////////
-
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        this.gestureDetector.onTouchEvent(event);
-        int a = (int)event.getX();
-        int a2 = (int)event.getY();
-        String s2 = String.valueOf(a2);
-        String s =  String.valueOf(a);
-        String s3 = s + " " + s2;
-        buckysMessage.setText(s3);
-        return super.onTouchEvent(event);
-    }
+//    @Override
+//    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onSingleTapConfirmed");
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onDoubleTap(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onDoubleTap");
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onDoubleTapEvent");
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onDown(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onDown");
+//        return false;
+//    }
+//
+//    @Override
+//    public void onShowPress(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onShowPress");
+//    }
+//
+//    @Override
+//    public boolean onSingleTapUp(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onSingleTapUp");
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+//        //buckysMessage.setText("onScroll");
+//        return false;
+//    }
+//
+//    @Override
+//    public void onLongPress(MotionEvent motionEvent) {
+//        //buckysMessage.setText("onLongPress");
+//    }
+//
+//    @Override
+//    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+//        //buckysMessage.setText("onFling");
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return false;
+//    }
+//
+//    ////////////////// End Gestures ///////////////////
+//
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        this.gestureDetector.onTouchEvent(event);
+//        try {
+//            int a = (int) event.getX();
+//            int a2 = (int) event.getY();
+//            String s2 = String.valueOf(a2);
+//            String s = String.valueOf(a);
+//            String s3 = s + " " + s2;
+//            v.setText(s3);
+//
+//        }
+//        catch (Exception e){}
+//        return super.onTouchEvent(event);
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
